180f187037281a992539a33e583e1f2364592b7e
Junlong Xie <decimalbell@gmail.com>
2022-10-28 21:36:59 +0800

    feat(list): add list command lindex

--- list.go
+++ list.go
@@ -2,10 +2,25 @@ package ssdb
 
 import (
 	"context"
+	"encoding/binary"
 	"errors"
 
 	"github.com/syndtr/goleveldb/leveldb"
 )
+
+const (
+	frontSeq uint64 = 2
+	backSeq  uint64 = 3
+)
+
+func encodeListKey(key []byte, seq uint64) []byte {
+	buf := make([]byte, 1+1+len(key)+8)
+	buf[0] = 'q'
+	buf[1] = byte(len(key))
+	copy(buf[2:], key)
+	binary.BigEndian.PutUint64(buf[1+1+len(key):], seq)
+	return buf
+}
 
 func encodeListLenKey(key []byte) []byte {
 	buf := make([]byte, 0, 1+len(key))
@@ -14,11 +29,33 @@ func encodeListLenKey(key []byte) []byte {
 	return buf
 }
 
-func (db *DB) LLen(ctx context.Context, key []byte) (int64, error) {
-	return db.llen(encodeListLenKey(key))
+func (db *DB) LIndex(ctx context.Context, key []byte, index int64) ([]byte, error) {
+	var (
+		seq int64
+		err error
+	)
+	if index >= 0 {
+		seq, err = db.lget(encodeListKey(key, frontSeq))
+		if err != nil {
+			return nil, err
+		}
+		seq += index
+	} else {
+		seq, err = db.lget(encodeListKey(key, backSeq))
+		if err != nil {
+			return nil, err
+		}
+		seq += index + 1
+	}
+
+	return db.get(encodeListKey(key, uint64(seq)))
 }
 
-func (db *DB) llen(ldbKey []byte) (int64, error) {
+func (db *DB) LLen(ctx context.Context, key []byte) (int64, error) {
+	return db.lget(encodeListLenKey(key))
+}
+
+func (db *DB) lget(ldbKey []byte) (int64, error) {
 	value, err := db.ldb.Get(ldbKey, nil)
 	if errors.Is(err, leveldb.ErrNotFound) {
 		return 0, nil
--- tests/list_test.go
+++ tests/list_test.go
@@ -23,3 +23,42 @@ func TestLLen(t *testing.T) {
 		assert.EqualValues(t, 20, val)
 	}
 }
+
+func TestLIndex(t *testing.T) {
+	db, err := ssdb.Open(dir, nil)
+	assert.Nil(t, err)
+	defer db.Close()
+
+	ctx := context.TODO()
+
+	for i := 0; i < 1; i++ {
+		key := []byte(fmt.Sprintf("list%d", i))
+		for j := 0; j < 10; j++ {
+			element := []byte(fmt.Sprintf("element%d", 9-j))
+			val, err := db.LIndex(ctx, key, int64(j))
+			assert.Nil(t, err)
+			assert.EqualValues(t, element, val)
+		}
+
+		for j := 10; j < 20; j++ {
+			element := []byte(fmt.Sprintf("element%d", j-10))
+			val, err := db.LIndex(ctx, key, int64(j))
+			assert.Nil(t, err)
+			assert.EqualValues(t, element, val)
+		}
+
+		for j := -1; j >= -10; j-- {
+			element := []byte(fmt.Sprintf("element%d", 10+j))
+			val, err := db.LIndex(ctx, key, int64(j))
+			assert.Nil(t, err)
+			assert.EqualValues(t, element, val)
+		}
+
+		for j := -11; j >= -20; j-- {
+			element := []byte(fmt.Sprintf("element%d", -(11 + j)))
+			val, err := db.LIndex(ctx, key, int64(j))
+			assert.Nil(t, err)
+			assert.EqualValues(t, element, val)
+		}
+	}
+}
